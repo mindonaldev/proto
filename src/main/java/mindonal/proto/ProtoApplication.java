d247464f23506c13bc7b8209a650449a18fcb526
bk.ko <bk.ko@sk.com>
2015-06-10 22:48:04 +0900

    init project

---
+++ src/main/java/mindonal/proto/ProtoApplication.java
@@ -0,0 +1,15 @@
+package mindonal.proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by bk.ko@sk.com on 6/10/15.
+ */
+@SpringBootApplication
+public class ProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoApplication.class, args);
+    }
+}
